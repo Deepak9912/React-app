bb8ea7667a4ba12f3a623e2eb0f3553a507c1317
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-07-04 20:06:57 +0000

    add form

--- taskbar/src/components/Expenses/ExpenseItem.js
+++ taskbar/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,23 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    //state
+    const [title, subTitle] = useState(props.title);
+    const clickHandler = () => { 
+        subTitle('Updated!');
+    }
+
     return <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button onClick={clickHandler}>Change title</button>
     </Card>
 }
 
