0fae5f7bda8353406d7adea12488893b1d154471
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-07-04 14:46:19 +0000

    static finished

--- taskbar/src/App.js
+++ taskbar/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
