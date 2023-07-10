3a223844a39b0fbc33681dc2a8ffa0fc17986f4f
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-07-10 19:16:20 +0000

    add oncancel btn

--- taskbar/src/components/NewExpense/NewExpense.js
+++ taskbar/src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewExpense.css';
 import './ExpenseForm';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    const [isEditing, setEditing] = useState(false)
     //below function will  save the data entered by user
     //data transfer from child to parent
     const saveExpenseDataHandler = (entedredExpenseData) => {
@@ -12,11 +13,21 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setEditing(false);
     };
 
-    return <div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+    const startEditingHandler = () => {
+        setEditing(true);
+    };
+
+    const stopEditingHandler = () => {
+        setEditing(false);
+    };
+
+    return (<div className='new-expense'>
+        {!isEditing && (<button onClick={startEditingHandler}>Add New Expense</button>)}
+        {isEditing && (<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />)}
+    </div>);
 };
 
 export default NewExpense;
